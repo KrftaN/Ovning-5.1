ea1c01273a864cf794359c9972c37c9dc7b0faa2
KraftaN <63770425+KrftaN@users.noreply.github.com>
2024-05-31 13:55:18 +0200

    Add project files.

---
+++ Ovning 5.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace övning_5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] meningar = {
+                "Det här är den första meningen.",
+                "Här kommer den andra meningen.",
+                "Och slutligen, den tredje meningen."
+            };
+
+            foreach (string mening in meningar)
+            {
+                Console.WriteLine(mening);
+                Console.WriteLine(); // Lägg till en tom rad mellan varje mening
+            }
+        }
+    }
+}
